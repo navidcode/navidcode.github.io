012368c03066a3970d0ee1de49f39550d58e22cb
navidcode <navid.fzy@gmail.com>
2024-09-11 18:57:40 -0400

    Added colors on right side of page
    Using html canvas, added some colors and movement using js on the right side of page

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
 function alertButton() {
     alert("Javascript test")
 }
+
+const canvas = document.querySelector('canvas')
+const c = canvas.getContext('2d')
+
+
+let y = 10
+let x = 0
+let z = 0
+
+// Animation Loop
+function animate() {
+    window.requestAnimationFrame(animate)
+
+    c.fillStyle = 'green'
+    c.fillRect(120, y, 5, 2)
+
+    c.fillStyle = 'cornflowerblue'
+    c.fillRect(160, y, 5, 10)
+
+    c.fillStyle = 'tomato'
+    c.fillRect(200, y, 5, 18)
+
+    y++
+}
+
+animate()
